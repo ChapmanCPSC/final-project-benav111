52e03e3eb6627f5426654f655bfafc0086dc1b28
zoidberg <benav111@llb12-imac-20.local>
2016-05-04 20:06:42 -0700

    Began project

---
+++ SwiftFinal/QuizClass.swift
@@ -0,0 +1,13 @@
+//
+//  QuizClass.swift
+//  SwiftFinal
+//
+//  Created by Benavidez, Amanda on 5/4/16.
+//  Copyright © 2016 Benavidez, Amanda. All rights reserved.
+//
+
+import UIKit
+
+class QuizClass {
+    
+}
