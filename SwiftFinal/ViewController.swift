561275b3e8f24e6375884e204e41b6e8228b0dbe
zoidberg <benav111@llb12-imac-20.local>
2016-05-04 19:16:30 -0700

    Initial Commit

---
+++ SwiftFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftFinal
+//
+//  Created by Benavidez, Amanda on 5/4/16.
+//  Copyright © 2016 Benavidez, Amanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
